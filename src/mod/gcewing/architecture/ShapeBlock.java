978adddd544422176ae728c5df7a6bb612aac36f
Sporkley <Sporkley@users.noreply.github.com>
2016-10-27 01:11:17 -0700

    fixes #3 (#4)
    * fixes #3
    
    * re-use existing method to generate cladding (from chisel use)

--- src/mod/gcewing/architecture/ShapeBlock.java
+++ src/mod/gcewing/architecture/ShapeBlock.java
@@ -173,7 +173,7 @@ public class ShapeBlock extends BaseBlock<ShapeTE> {
 			//System.out.printf("ShapeBlock.harvestBlock: spawning %s\n", stack);
 			spawnAsEntity(world, pos, stack);
 			if (ste.secondaryBlockState != null) {
-				stack = BaseUtils.blockStackWithState(ste.secondaryBlockState, 1);
+				stack = ste.shape.kind.newSecondaryMaterialStack(ste.secondaryBlockState);
 				//System.out.printf("ShapeBlock.harvestBlock: spawning %s\n", stack);
 				spawnAsEntity(world, pos, stack);
 			}
