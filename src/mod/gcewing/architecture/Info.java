f174f72b2fb7dc4d09c382fa5e6a9c2332297303
Gregory Ewing <greg.ewing@canterbury.ac.nz>
2017-06-18 13:10:39 +1200

    Version info update

--- src/mod/gcewing/architecture/Info.java
+++ src/mod/gcewing/architecture/Info.java
@@ -6,7 +6,7 @@ public class Info {
 	static final String modName = "ArchitectureCraft";
 	static final String modID = "architecturecraft";
 	//static final String modPackage = "";
-	static final String versionNumber = "1.7.1";
+	static final String versionNumber = "1.7.3";
 	static final String versionBounds = "[1.7,1.8)";
 	static final String acceptedMinecraftVersions = "[1.10.2,1.10.2]";
 }
